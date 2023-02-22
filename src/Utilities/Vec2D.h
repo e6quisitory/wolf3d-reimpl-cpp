798c337fd0b66b1eeb60b5bdce0b7de09d2c0173
e6quisitory <mhamzaqayyum@gmail.com>
2023-02-21 20:08:01 -0800

    removed out of line Vec2D definitions ; should compile on GCC, MSVC now

--- src/Utilities/Vec2D.h
+++ src/Utilities/Vec2D.h
@@ -18,32 +18,90 @@ public:
     T e[2];
 
 public:
-    Vec2D(const T x, const T y);
-    Vec2D(const T a);
-    Vec2D();
+    /*
+    =================================================
+        Constructors
+    =================================================
+    */
+
+    Vec2D(const T x, const T y) : e{x, y} {}
+    Vec2D(const T a)            : e{a, a} {}
+    Vec2D()                     : e{static_cast<T>(0), static_cast<T>(0)} {}
 
     template<typename U>
-    Vec2D(const Vec2D<U>& v);
+    Vec2D(const Vec2D<U>& v)    : e{static_cast<T>(v.x()), static_cast<T>(v.y())} {}
 
-    Vec2D<int>(const xDir_t xDir);
-    Vec2D<int>(const yDir_t yDir);
+    Vec2D(const xDir_t xDir)    : e{static_cast<int>(xDir), 0} {}
+    Vec2D(const yDir_t yDir)    : e{0, static_cast<int>(yDir)} {}
 
-    T& x();
-    T& y();
-    T x() const;
-    T y() const;
+    /*
+    =================================================
+        Getters / Setters
+    =================================================
+    */
 
-    T&         operator  []  (const int index);
-    T          operator  []  (const int index)     const;
-    Vec2D<T>&  operator  +=  (const Vec2D<T>& v);
-    Vec2D<T>&  operator  -=  (const Vec2D<T>& v);
-    Vec2D<T>&  operator  *=  (const double d);
-    bool       operator  ==  (const Vec2D& v)      const;
-    bool       operator  !=  (const Vec2D& v)      const;
+    T& x()         { return e[0]; }
+    T& y()         { return e[1]; }
+    T  x()  const  { return e[0]; }
+    T  y()  const  { return e[1]; }
 
-    double         LengthSquared()                         const;
-    double         Length()                                const;
-    Vec2D<double>  Rotate          (const double radians)  const;
+    /*
+    =================================================
+        lvalue Operators
+    =================================================
+    */
+
+    T& operator [] (const int index) {
+        return e[index];
+    }
+
+    T operator [] (const int index) const {
+        return e[index];
+    }
+
+    Vec2D<T>& operator += (const Vec2D<T>& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        return *this;
+    }
+
+    Vec2D<T>& operator -= (const Vec2D<T>& v) {
+        e[0] -= v.e[0];
+        e[1] -= v.e[1];
+        return *this;
+    }
+
+    Vec2D<T>& operator *= (const double d) {
+        e[0] *= d;
+        e[1] *= d;
+        return *this;
+    }
+
+    bool operator == (const Vec2D& v) const {
+        return e[0] == v.e[0] && e[1] == v.e[1] ? true : false;
+    }
+
+    bool operator != (const Vec2D& v) const {
+        return (*this) == v ? false : true;
+    }
+
+    /*
+    =================================================
+        Member Methods
+    =================================================
+    */
+
+    double LengthSquared() const {
+        return e[0]*e[0] + e[1]*e[1];
+    }
+
+    double Length() const {
+        return std::sqrt(LengthSquared());
+    }
+
+    Vec2D<double> Rotate(const double radians) const {
+        return Vec2D<double>(e[0] * std::cos(radians) - e[1] * std::sin(radians), e[0] * std::sin(radians) + e[1] * std::cos(radians));
+    }
 };
 
 /*
@@ -57,110 +115,6 @@ typedef  Vec2D <int>     iPoint2;
 typedef  Vec2D <double>  Vec2;
 typedef  Vec2D <int>     iVec2;
 typedef  Vec2D <int>     Pixel;
-
-/*
-=================================================
-    Constructors
-=================================================
-*/
-
-template<typename T>
-    Vec2D<T>::Vec2D(const T x, const T y):
-        e{x, y} {}
-
-template<typename T>
-    Vec2D<T>::Vec2D(const T a):
-        e{a, a} {}
-
-template<typename T>
-    Vec2D<T>::Vec2D():
-        e{static_cast<T>(0), static_cast<T>(0)} {}
-
-template<typename T>
-    template<typename U>
-        Vec2D<T>::Vec2D(const Vec2D<U>& v):
-            e{static_cast<T>(v.x()), static_cast<T>(v.y())} {}
-
-template<typename T>
-    Vec2D<T>::Vec2D(const xDir_t xDir):
-        e{static_cast<int>(xDir), 0} {}
-
-template<typename T>
-Vec2D<T>::Vec2D(const yDir_t yDir):
-        e{0, static_cast<int>(yDir)} {}
-
-/*
-=================================================
-    Getters / Setters
-=================================================
-*/
-
-template<typename T>
-    T& Vec2D<T>::x() {
-        return e[0];
-    }
-
-template<typename T>
-    T& Vec2D<T>::y() {
-        return e[1];
-    }
-
-template<typename T>
-    T Vec2D<T>::x() const {
-        return e[0];
-    }
-
-template<typename T>
-    T Vec2D<T>::y() const {
-        return e[1];
-    }
-
-/*
-=================================================
-    lvalue Operators
-=================================================
-*/
-
-template<typename T>
-    T& Vec2D<T>::operator [] (const int index) {
-        return e[index];
-    }
-
-template<typename T>
-    T Vec2D<T>::operator [] (const int index) const {
-        return e[index];
-    }
-
-template<typename T>
-    Vec2D<T>& Vec2D<T>::operator += (const Vec2D<T>& v) {
-        e[0] += v.e[0];
-        e[1] += v.e[1];
-        return *this;
-    }
-
-template<typename T>
-    Vec2D<T>& Vec2D<T>::operator -= (const Vec2D<T>& v) {
-        e[0] -= v.e[0];
-        e[1] -= v.e[1];
-        return *this;
-    }
-
-template<typename T>
-    Vec2D<T>& Vec2D<T>::operator *= (const double d) {
-        e[0] *= d;
-        e[1] *= d;
-        return *this;
-    }
-
-template<typename T>
-    bool Vec2D<T>::operator == (const Vec2D& v) const {
-        return e[0] == v.e[0] && e[1] == v.e[1] ? true : false;
-    }
-
-template<typename T>
-    bool Vec2D<T>::operator != (const Vec2D& v) const {
-        return (*this) == v ? false : true;
-    }
 
 /*
 =================================================
@@ -209,27 +163,6 @@ template<typename T>
 template<typename T>
     inline Vec2D<T> operator / (const Vec2D<T>& v, const double t) {
         return Vec2D<T>(v.x() / t, v.y() / t);
-    }
-
-/*
-=================================================
-    Member Methods
-=================================================
-*/
-
-template<typename T>
-    double Vec2D<T>::LengthSquared() const {
-        return e[0]*e[0] + e[1]*e[1];
-    }
-
-template<typename T>
-    double Vec2D<T>::Length() const {
-        return std::sqrt(LengthSquared());
-    }
-
-template<typename T>
-    Vec2D<double> Vec2D<T>::Rotate(const double radians) const {
-        return Vec2D<double>(e[0] * std::cos(radians) - e[1] * std::sin(radians), e[0] * std::sin(radians) + e[1] * std::cos(radians));
     }
 
 /*
